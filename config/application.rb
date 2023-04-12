fdc69dc6269f5f3722e85ecb15bb59e962d2dcaf
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-04-12 18:35:05 +0300

    create local environmental variables

--- config/application.rb
+++ config/application.rb
@@ -18,12 +18,12 @@ module RORMpesa
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-  end
-
-  config.before_configuration do
-    env_file = File.join(Rails.root, 'config', 'local_env.yml')
-    YAML.load(File.open(env_file)).each do |key, value|
-      ENV[key.to_s] = value
-    end if File.exists?(env_file)
+    config.api_only = true
+    config.before_configuration do
+      env_file = File.join(Rails.root, "config", "local_env.yml")
+      YAML
+        .load(File.open(env_file))
+        .each { |key, value| ENV[key.to_s] = value } if File.exists?(env_file)
+    end
   end
 end
